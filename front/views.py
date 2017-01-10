801b23e2bc4cb2f5f6b10fd65ec31fbaa68e85dc
D3Exclusive <eduffy2@student.fitchburgstate.edu>
2017-01-10 11:12:23 -0500

    Added template support and added basic home html file

--- front/views.py
+++ front/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello world")
+    template = loader.get_template('front/home.html')
+    context = {
+        'title' : 'Welcome to our Worlds',
+    }
+    return HttpResponse(template.render(context, request))
