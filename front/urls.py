3cb4e1ce8f9d236f951c491a4baa63bc917b44bf
D3Exclusive <eduffy2@student.fitchburgstate.edu>
2017-01-10 22:00:59 -0500

    Added template inheritance and started static file management

--- front/urls.py
+++ front/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from django.template import Template, Context
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name="home"),
-]
+    url(r'^', views.home, name="home"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
